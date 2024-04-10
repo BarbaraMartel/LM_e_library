a3b74748916a0e60b447b176b9dddd6c33750670
BarbaraMartel <barbaramartel7@gmail.com>
2024-04-10 16:19:44 -0500

    primer commit

---
+++ screens/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class SearchScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text> Pantalla de Búsqueda</Text>
+            </View>
+        );
+    }
+}
+
+const style = StyleSheet.create(
+    
+);
